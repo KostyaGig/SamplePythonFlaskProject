d4c801a92833aa3128a62a04125dac811049196e
Kostya Zinoview <k.gig@list.ru>
2023-03-19 12:41:24 +0300

    add validation for login inputs

--- authentication/validation.py
+++ authentication/validation.py
@@ -3,5 +3,8 @@
 """
 
 
-def validate_register(email, name, password):
+def validate_register_inputs(email, name, password):
     return (email and '@' in email) and name and (password and len(password) > 4)
+
+def validate_login_inputs(email, password):
+    return (email and '@' in email) and (password and len(password) > 4)
