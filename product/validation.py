4430546f711a9218e3e5e0f58da0053b40407b73
Kostya Zinoview <k.gig@list.ru>
2023-03-26 12:47:18 +0300

    add validation for product inputs

---
+++ product/validation.py
@@ -0,0 +1,19 @@
+from product.product_status import ProductStatus
+
+
+def valid_post_product(description, title):
+    return description and title
+
+
+def valid_update_product(description, title, product_id):
+    return valid_post_product(description, title) and product_id >= 0
+
+
+def check_status(status):
+    return status == ProductStatus.ON_REVIEW.value \
+           or status == ProductStatus.APPROVED.value \
+           or status == ProductStatus.DENIED.value
+
+
+def valid_delete_product(product_id):
+    return product_id >= 0
